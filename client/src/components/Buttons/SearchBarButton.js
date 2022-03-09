196c9bb8883ac19f22e89e556276ab2d2c4d4651
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-09 13:51:16 -0300

    button styles import added

---
+++ client/src/components/Buttons/SearchBarButton.js
@@ -0,0 +1,27 @@
+export const SearchBarButton = `
+  display: flex;
+  align-self: center;
+  margin-left: 5px;
+  box-shadow: inset 0px 0px 0px 0px #91b8b3;
+  background: linear-gradient(to bottom, #768d87 5%, #6c7c7c 100%);
+  background-color: #768d87;
+  border-radius: 15px;
+  border: 1px solid #566963;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: "ABeeZee";
+  font-size: 16px;
+  padding: 1px 4px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #2b665e;
+
+  &:hover {
+    background: linear-gradient(to bottom, #6c7c7c 5%, #768d87 100%);
+    background-color: #6c7c7c;
+  }
+
+  &:active {
+    position: relative;
+    top: 1px;
+  }
+`;
