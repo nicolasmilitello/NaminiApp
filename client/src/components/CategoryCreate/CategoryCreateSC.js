7c980841c37489bcf0ee6612188ec5a5a5c568b1
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-11 11:56:50 -0300

    glass styles imported

--- client/src/components/CategoryCreate/CategoryCreateSC.js
+++ client/src/components/CategoryCreate/CategoryCreateSC.js
@@ -4,6 +4,7 @@ import {
   disabledSaveChangesButton,
   saveChangesButton,
 } from "../Buttons/SaveChangesButton";
+import { glassStyle } from "../GlassStyle/GlassStyle";
 
 export const Container = styled.div`
   display: flex;
@@ -12,22 +13,14 @@ export const Container = styled.div`
 `;
 
 export const CreatorCard = styled.div`
-  display: flex;
-  justify-content: flex-start;
+  ${glassStyle}
+
+  margin-top: 20px;
+  margin-bottom: 0px;
   flex-direction: column;
-  align-items: center;
   width: 700px;
   box-sizing: content-box;
   padding-bottom: 20px;
-  margin-top: 20px;
-  margin-bottom: 0px;
-  border-radius: 10px;
-  backdrop-filter: blur(8px);
-  background-color: rgba(242, 242, 205, 0.075);
-  box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 8px;
-  border: 2px rgba(255, 255, 255, 0.4) solid;
-  border-bottom: 2px rgba(40, 40, 40, 0.35) solid;
-  border-right: 2px rgba(40, 40, 40, 0.35) solid;
 
   & h1 {
     width: 100%;
