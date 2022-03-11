335fe54cff5db7dbd03f66c543da2eac3bc3dac8
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-10 23:03:22 -0300

    EditItemButton styles created

--- client/src/components/Buttons/EditItemButton.js
+++ client/src/components/Buttons/EditItemButton.js
@@ -1,19 +1,23 @@
+import { activePseudoClass } from "./SharedProps";
+
 export const editItemButton = `
   display: flex;
-  align-self: center;
   margin-left: 5px;
-  border-radius: 15px;
-  cursor: pointer;
-  color: #ffffff;
+
   font-size: 12px;
-  padding: 1px 4px;
-  text-decoration: none;
+  color: #ffffff;
   
+  border-radius: 13px;
+
+  cursor: pointer;
+
   box-shadow: inset 0px 1px 0px 0px #276873;
   background: linear-gradient(to bottom, #599bb3 5%, #408c99 100%);
   background-color: #599bb3;
   border: 1px solid #29668f;
   text-shadow: 0px 1px 0px #2b665e;
+  padding: 1px 4px;
+  
 
   &:hover {
     background: linear-gradient(to bottom, #408c99 5%, #599bb3 100%);
@@ -21,7 +25,6 @@ export const editItemButton = `
   }
 
   &:active {
-    position: relative;
-    top: 1px;
+    ${activePseudoClass}
   }
   `;
