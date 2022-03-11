c33b72121c9c7e7ac71387d777d8ebcd7354bde2
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-11 11:56:23 -0300

    glass styles imported

--- client/src/components/Card/CardSC.js
+++ client/src/components/Card/CardSC.js
@@ -1,19 +1,12 @@
 import styled from "styled-components";
+import { glassStyle } from "../GlassStyle/GlassStyle";
 
 export const Container = styled.div`
-  display: flex;
+  ${glassStyle}
+
   width: 300px;
   height: 180px;
   margin: 10px;
-  justify-content: flex-start;
-  align-items: center;
-  border-radius: 10px;
-  backdrop-filter: blur(8px);
-  background-color: rgba(242, 242, 205, 0.075);
-  box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 8px;
-  border: 2px rgba(255, 255, 255, 0.4) solid;
-  border-bottom: 2px rgba(40, 40, 40, 0.35) solid;
-  border-right: 2px rgba(40, 40, 40, 0.35) solid;
   overflow: hidden;
 
   &:hover {
