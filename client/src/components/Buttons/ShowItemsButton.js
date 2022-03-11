8ddf84049ba6bb7829d90d932b952886d2459496
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-10 23:06:13 -0300

    ShowItemsButton styles created

--- client/src/components/Buttons/ShowItemsButton.js
+++ client/src/components/Buttons/ShowItemsButton.js
@@ -1,23 +1,15 @@
-import { sharedProps } from "./SaveChangesButton";
+import {
+  saveShowItemsSharedProps,
+  searchReloadShowItemsSharedProps,
+  activePseudoClass,
+} from "./SharedProps";
 
 export const showItemsButton = `
-  ${sharedProps}
+  ${saveShowItemsSharedProps}
 
-  box-shadow: inset 0px 0px 0px 0px #91b8b3;
-  background: linear-gradient(to bottom, #768d87 5%, #6c7c7c 100%);
-  background-color: #768d87;
-  border: 1px solid #566963;
-  cursor: pointer;
-  color: #ffffff;
-  text-shadow: 0px 1px 0px #2b665e;
-
-  &:hover {
-    background: linear-gradient(to bottom, #6c7c7c 5%, #768d87 100%);
-    background-color: #6c7c7c;
-  }
+  ${searchReloadShowItemsSharedProps}
 
   &:active {
-    position: relative;
-    top: 1px;
+    ${activePseudoClass}
   }
 `;
