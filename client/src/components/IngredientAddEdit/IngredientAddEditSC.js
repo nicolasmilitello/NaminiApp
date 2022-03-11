173a97a479dbd4b8b5f776cfb4c2898f75ff7ec9
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-10 23:09:20 -0300

    button styles imported

--- client/src/components/IngredientAddEdit/IngredientAddEditSC.js
+++ client/src/components/IngredientAddEdit/IngredientAddEditSC.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { disabledPlusButton, plusButton } from "../Buttons/PlusButtons";
 
 export const FormContainer = styled.form`
   display: flex;
@@ -38,53 +39,11 @@ export const Unit = styled.span`
 `;
 
 export const AddIngrButton = styled.button`
-  display: flex;
-  align-items: center;
-  height: 17px;
-  font-size: 15px;
-  padding: 0px 0px;
-  margin-left: 1%;
-  box-shadow: inset 0px 0px 2px 0px #3dc21b;
-  background: linear-gradient(to bottom, #23b84a 5%, #4d9925 100%);
-  background-color: #23b84a;
-  border-radius: 13px;
-  border: 1px solid #18ab29;
-  cursor: pointer;
-  color: #ffffff;
-  font-family: var(--secondaryFont);
-  font-weight: bold;
-  text-decoration: none;
-  text-shadow: 0px 0px 0px #2f6627;
-
-  &:hover {
-    background: linear-gradient(to bottom, #4d9925 5%, #23b84a 100%);
-    background-color: #4d9925;
-  }
-
-  &:active {
-    position: relative;
-    top: 1px;
-  }
+  ${plusButton}
 `;
 
 export const DisabledAddButton = styled.button`
-  display: flex;
-  align-items: center;
-  height: 17px;
-  font-size: 15px;
-  padding: 0px 0px;
-  margin-left: 1%;
-  box-shadow: inset 0px 1px 0px 0px #ffffff;
-  background: linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);
-  background-color: #ffffff;
-  border-radius: 13px;
-  border: 1px solid #dcdcdc;
-  cursor: default;
-  color: #666666;
-  font-family: var(--secondaryFont);
-  font-weight: bold;
-  text-decoration: none;
-  text-shadow: 0px 1px 0px #ffffff;
+  ${disabledPlusButton}
 `;
 
 export const IngrError = styled.p`
