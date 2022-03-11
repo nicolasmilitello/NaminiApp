6bd0904fc505efcf8cfe2c1d89518f0fe4c3f189
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-10 23:02:44 -0300

    DeleteConfirmedItemButton styles created

--- client/src/components/Buttons/DeleteConfirmedItemButton.js
+++ client/src/components/Buttons/DeleteConfirmedItemButton.js
@@ -1,23 +1,23 @@
-import { sharedProps } from "./PlusButtons";
+import {
+  deletePlusSharedProps,
+  deleteSharedProps,
+  activePseudoClass,
+} from "./SharedProps";
 
 export const deleteConfirmedItemButton = `
+  ${deletePlusSharedProps}
 
-  ${sharedProps}
-    margin-left: 5px;
-    box-shadow: inset 0px 0px 2px 0px #cf866c;
-    background: linear-gradient(to bottom, #d0451b 5%, #bc3315 100%);
-    background-color: #d0451b;
-    border: 1px solid #942911;
-    cursor: pointer;
-    color: #ffffff;
+  margin-left: 5px;
 
-    &:hover {
-      background: linear-gradient(to bottom, #bc3315 5%, #d0451b 100%);
-      background-color: #bc3315;
-    }
+  color: #ffffff;
+
+  cursor: pointer;
+
+  box-shadow: inset 0px 1px 0px 0px #cf866c;
+
+  ${deleteSharedProps}
 
     &:active {
-      position: relative;
-      top: 1px;
+      ${activePseudoClass}
     }
 `;
