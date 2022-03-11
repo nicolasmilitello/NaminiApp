fc5bb00b1069bc1577d9bec2937863ee325bda05
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-11 11:54:43 -0300

    GlassStyle folder and file created

---
+++ client/src/components/GlassStyle/GlassStyle.js
@@ -0,0 +1,12 @@
+export const glassStyle = `
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  border-radius: 10px;
+  backdrop-filter: blur(8px);
+  background-color: rgba(242, 242, 205, 0.075);
+  box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 8px;
+  border: 2px rgba(255, 255, 255, 0.4) solid;
+  border-bottom: 2px rgba(40, 40, 40, 0.35) solid;
+  border-right: 2px rgba(40, 40, 40, 0.35) solid;
+`;
