66c60195d6a7c67636b1f1e39ce45ce6e0ca6d8d
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-11 12:00:52 -0300

    glass styles imported

--- client/src/components/NameEdit/NameEditSC.js
+++ client/src/components/NameEdit/NameEditSC.js
@@ -5,6 +5,7 @@ import {
   saveChangesButton,
 } from "../Buttons/SaveChangesButton";
 import { returnButton } from "../Buttons/ReturnButton";
+import { glassStyle } from "../GlassStyle/GlassStyle";
 
 export const Container = styled.div`
   display: flex;
@@ -14,20 +15,12 @@ export const Container = styled.div`
 `;
 
 export const Card = styled.div`
-  display: flex;
-  justify-content: flex-start;
+  ${glassStyle}
+
   flex-direction: column;
-  align-items: center;
   width: 50%;
   height: 140px;
   margin-bottom: 0px;
-  border-radius: 10px;
-  backdrop-filter: blur(8px);
-  background-color: rgba(242, 242, 205, 0.075);
-  box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 8px;
-  border: 2px rgba(255, 255, 255, 0.4) solid;
-  border-bottom: 2px rgba(40, 40, 40, 0.35) solid;
-  border-right: 2px rgba(40, 40, 40, 0.35) solid;
   padding-bottom: 15px;
 
   & h1 {
