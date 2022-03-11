ce79939c4044175840d280785345f194161b5559
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-11 12:00:27 -0300

    glass styles imported

--- client/src/components/IngredientsEdit/IngredientsEditSC.js
+++ client/src/components/IngredientsEdit/IngredientsEditSC.js
@@ -6,6 +6,7 @@ import {
 } from "../Buttons/SaveChangesButton";
 import { returnButton } from "../Buttons/ReturnButton";
 import { deleteConfirmedItemButton } from "../Buttons/DeleteConfirmedItemButton";
+import { glassStyle } from "../GlassStyle/GlassStyle";
 
 export const Container = styled.div`
   display: flex;
@@ -15,21 +16,12 @@ export const Container = styled.div`
 `;
 
 export const Card = styled.div`
-  display: flex;
-  justify-content: flex-start;
+  ${glassStyle}
+
   flex-direction: column;
-  align-items: center;
   width: 50%;
   box-sizing: content-box;
   margin-bottom: 0px;
-  border-radius: 10px;
-  backdrop-filter: blur(8px);
-  background-color: rgba(242, 242, 205, 0.075);
-  box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 8px;
-  border: 2px rgba(255, 255, 255, 0.4) solid;
-  border-bottom: 2px rgba(40, 40, 40, 0.35) solid;
-  border-right: 2px rgba(40, 40, 40, 0.35) solid;
-  padding-bottom: 15px;
 
   & h1 {
     width: 100%;
