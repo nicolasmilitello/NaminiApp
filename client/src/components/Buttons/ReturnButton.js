973c6a470d5300e269ef9b6c6069d67f3ae23fd0
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-10 23:04:48 -0300

    ReturnButton styles created

--- client/src/components/Buttons/ReturnButton.js
+++ client/src/components/Buttons/ReturnButton.js
@@ -1,8 +1,9 @@
-import { someProps } from "./EditRecipeButton";
+import { deleteEditReturnSharedProps, activePseudoClass } from "./SharedProps";
 
 export const returnButton = `
-  ${someProps}
-  box-shadow: inset 0px 0px 0px 0px #91b8b3;
+  ${deleteEditReturnSharedProps}
+  
+  box-shadow: inset 0px 1px 0px 0px #0a3d36;
   background: linear-gradient(to bottom, #768d87 5%, #6c7c7c 100%);
   background-color: #768d87;
   border: 1px solid #566963;
@@ -13,7 +14,6 @@ export const returnButton = `
   }
 
   &:active {
-    position: relative;
-    top: 1px;
+    ${activePseudoClass}
   }
 `;
