1cc346033d0dd0673aebec25abe3b5d704177410
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-11 11:57:57 -0300

    glass styles imported

--- client/src/components/Home/HomeSC.js
+++ client/src/components/Home/HomeSC.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { ScaleDownCenter } from "../Animations/ScaleDownCenter";
+import { glassStyle } from "../GlassStyle/GlassStyle";
 
 export const Container = styled.div`
   display: flex;
@@ -9,19 +10,12 @@ export const Container = styled.div`
 `;
 
 export const FiltersTitleAndSearchBarContainer = styled.div`
-  display: flex;
+  ${glassStyle}
+
   flex-direction: row;
   justify-content: space-between;
-  align-items: center;
   width: 70%;
   margin-top: 20px;
-  border-radius: 10px;
-  backdrop-filter: blur(8px);
-  background-color: rgba(242, 242, 205, 0.075);
-  box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 8px;
-  border: 2px rgba(255, 255, 255, 0.4) solid;
-  border-bottom: 2px rgba(40, 40, 40, 0.35) solid;
-  border-right: 2px rgba(40, 40, 40, 0.35) solid;
 
   & h1 {
     width: 75%;
