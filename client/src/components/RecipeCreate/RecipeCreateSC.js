efd9d8367f417dbff2ee5ce81ce5a3a87f1c5161
nicolasmilitello <nicolas.militello@hotmail.com>
2022-03-11 12:01:34 -0300

    glass styles imported

--- client/src/components/RecipeCreate/RecipeCreateSC.js
+++ client/src/components/RecipeCreate/RecipeCreateSC.js
@@ -5,6 +5,7 @@ import {
   disabledSaveChangesButton,
   saveChangesButton,
 } from "../Buttons/SaveChangesButton";
+import { glassStyle } from "../GlassStyle/GlassStyle";
 
 export const Container = styled.div`
   display: flex;
@@ -12,21 +13,13 @@ export const Container = styled.div`
 `;
 
 export const Card = styled.div`
-  display: flex;
-  justify-content: flex-start;
+  ${glassStyle}
+
   flex-direction: column;
-  align-items: center;
   width: 55%;
   box-sizing: content-box;
   margin-top: 20px;
   margin-bottom: 0px;
-  border-radius: 10px;
-  backdrop-filter: blur(8px);
-  background-color: rgba(242, 242, 205, 0.075);
-  box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 8px;
-  border: 2px rgba(255, 255, 255, 0.4) solid;
-  border-bottom: 2px rgba(40, 40, 40, 0.35) solid;
-  border-right: 2px rgba(40, 40, 40, 0.35) solid;
   padding-bottom: 10px;
 
   & h1 {
